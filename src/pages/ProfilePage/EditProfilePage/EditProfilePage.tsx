0f40b7d5fed6596bfa1263d9fd781db27a1aec21
alvihelene <95623140+alvihelene@users.noreply.github.com>
2024-05-08 11:03:39 +0200

    EditProfilePage in progress. Adding the input field at the moment

--- src/pages/ProfilePage/EditProfilePage/EditProfilePage.tsx
+++ src/pages/ProfilePage/EditProfilePage/EditProfilePage.tsx
@@ -1,14 +1,20 @@
-import Header from '../../../components/shared/header/header';
-import './EditProfilePage';
-
+import Header from "../../../components/shared/header/header";
+import "./EditProfilePage.scss";
 
 const EditProfilePage = () => {
   return (
     <>
-    <Header currentPage="Rediger profil" />
-    <div>
-      <h2>Rediger din profil</h2>
-    </div>
+      <Header currentPage="Rediger profil" />
+      <div className="EditProfilePage">
+        <h2>Rediger din profil</h2>
+        <div className="EditProfilePage_Form">
+        <input
+            type="text"
+            placeholder="Navn"
+            onChange={(e) => setName(e.target.value)}
+          />
+        </div>
+      </div>
     </>
   );
 };
