46dfd066193e414db4209cc8e371e940ac5f350c
Artaey <95622783+Artaey@users.noreply.github.com>
2024-05-09 11:48:29 +0200

    made swipaeble look good and work
    Copied some code from a library so I could change the styling

--- src/components/prize/prize.tsx
+++ src/components/prize/prize.tsx
@@ -1,9 +1,6 @@
-import React, { useEffect, useState } from "react";
 import "./prize.scss";
 import { IPrize } from "../../interfaces/IPrize";
 import Mønt from "../../assets/Mønt.png";
-import { useStore } from "../../stores/store";
-import { toast, Toaster, useToasterStore } from "react-hot-toast";
 import { useNavigate } from "react-router-dom";
 
 const Prize = ({ name, price, imageUrl }: IPrize) => {
@@ -12,33 +9,12 @@ const Prize = ({ name, price, imageUrl }: IPrize) => {
   };
 
   const navigate = useNavigate();
-  const { treasureStore, authStore } = useStore();
 
   const handlePay = () => {
     navigate("/pay");
-    // console.log(authStore.user?.points + " " + price);
-    // if (price <= authStore.user!.points) {
-    //   handleUpdatePoints(-price);
-    // }else{
-    //   toast.error("Du har ikke nok mønter", { duration: 2000 });
-    //   console.log("øv");
-    // }
   }
-  
-  const { toasts } = useToasterStore();
-  const TOAST_LIMIT = 1;
-  useEffect(() => {
-    toasts
-      .filter((t) => t.visible)
-      .filter((_, i) => i >= TOAST_LIMIT)
-      .forEach((t) => toast.dismiss(t.id));
-  }, [toasts]);
 
-  const handleUpdatePoints = async (points: number) => {
-    const response = await treasureStore.updatePoints(authStore.user!.id, points);
-    await authStore.getUser(authStore.user!.id);
-    console.log(response.totalPoints);
-  }
+  
 
   return (
     <div onClick={handlePay} className="Prize" style={{ backgroundImage: `url(${imageUrl})` }}>
@@ -53,7 +29,7 @@ const Prize = ({ name, price, imageUrl }: IPrize) => {
           <p className="Prize_Info_Text_Price">{getCoinText(price)}</p>
         </div>
       </div>
-      <Toaster />
+      
     </div>
   );
 };
--- src/pages/PayPage/PayPage.tsx
+++ src/pages/PayPage/PayPage.tsx
@@ -1,14 +1,47 @@
+import React, { useEffect } from "react";
 import Header from "../../components/shared/header/header";
 import "./PayPage.scss";
 import { useNavigate } from "react-router-dom";
-import { SwipeableButton } from "react-swipeable-button";
+import  SwipeableButton  from "../../components/shared/swipe/SwipeableButton"
+import { toast, Toaster, useToasterStore } from "react-hot-toast";
+import { useStore } from "../../stores/store";
 
-const TreasureHuntPage = () => {
+const PayPage = () => {
+
+  const price = 35
   const navigate = useNavigate();
+  const { treasureStore, authStore } = useStore();
   
   const handleBack = () => {
     navigate(-1);
+  }
+
+  const handlePay = () => {
+    console.log(authStore.user?.points + " " + price);
+    if (price <= authStore.user!.points) {
+      handleUpdatePoints(-price);
+      navigate(-1);
+    }else{
+      toast.error("Du har ikke nok mønter", { duration: 2000 });
+      console.log("øv");
+    }
   };
+
+  const handleUpdatePoints = async (points: number) => {
+    const response = await treasureStore.updatePoints(authStore.user!.id, points);
+    await authStore.getUser(authStore.user!.id);
+    console.log(response.totalPoints);
+  }
+
+  const { toasts } = useToasterStore();
+  const TOAST_LIMIT = 1;
+  useEffect(() => {
+    toasts
+      .filter((t) => t.visible)
+      .filter((_, i) => i >= TOAST_LIMIT)
+      .forEach((t) => toast.dismiss(t.id));
+  }, [toasts]);
+  
 
   return (
     <>
@@ -21,22 +54,23 @@ const TreasureHuntPage = () => {
             Skarø is
           </div>
           <div className="bold Orange_Text">
-            35 Eventyrmønter
+            {price} Eventyrmønter
           </div>
           <div className="PayPage_ContentContainer_Text">
             Nyd en forfriskende kugle is fra Skarø! Denne lækre præmie gælder for 2x is med én kugle. Præmien kan indløses i Geografisk Haves café, og køb kan ikke fortrydes.
           </div>
-
-           <SwipeableButton
-                onSuccess={() => console.log("bob")}
-                text="Brug Eventyrmønter"
-                text_unlocked="Betalt"
-           />
-
+          <div className="Swipe_Container">
+            <SwipeableButton
+                  onSuccess={handlePay}
+                  text="Brug Eventyrmønter"
+                  text_unlocked="Betalt"
+            />
+          </div>
         </div>
+        <Toaster />
       </div>
     </>
   );
 };
 
-export default TreasureHuntPage;
+export default PayPage;
